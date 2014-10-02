360f3e2f8d4ab2f275e4682ad4dbfed1818012b9
Simon Bengtsson <simonbengt@gmail.com>
2014-10-02 21:49:28 +0200

    Minor header improvements

--- src/main/webapp/main.js
+++ src/main/webapp/main.js
@@ -1,6 +1,11 @@
 
 var app = angular.module('hage', []);
 
-app.controller('MainCtrl', ['$scope', function($scope) {
-    console.log('sdf');
-}]); 
+app.constant('API_URL', 'http://localhost:8080/hage-1.0-SNAPSHOT/api');
+
+app.controller('MainCtrl', ['$scope', '$http', 'API_URL', function($scope, $http, API_URL) {
+    $scope.posts = [];
+    $http.get(API_URL + '/posts').success(function(res) {
+        $scope.posts = res;
+    });
+}]);
