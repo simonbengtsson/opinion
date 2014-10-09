ff966b0c97311f62761b37411aac27d42eb3e91a
ckabat <carolinekabat@hotmail.com>
2014-10-09 12:00:59 +0200

    First version of showing user profile + posts

--- src/main/webapp/js/services/ModelService.js
+++ src/main/webapp/js/services/ModelService.js
@@ -5,5 +5,13 @@ app.service('ModelService', [function () {
     this.posts = [
         {content: 'This is a hage', hates: 100}
     ];
+    this.users = [{ 
+        firstname: 'Caroline', 
+        posts: [
+            {content: 'Jag hatar alla!!!', hates: 23}, 
+            {content: '... när man försover sig :(:(:(:( :@ !!!', hates: 1234}
+            ]
+        }
+    ];
 
 }]);
