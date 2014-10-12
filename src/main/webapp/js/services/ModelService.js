c7227f018e31f3b4d3060fd631600f98dd92ba0c
ckabat <carolinekabat@gmail.com>
2014-10-13 00:22:20 +0200

    Profile layout

--- src/main/webapp/js/services/ModelService.js
+++ src/main/webapp/js/services/ModelService.js
@@ -8,9 +8,10 @@ app.service('ModelService', [function () {
     this.users = [{ 
         firstname: 'Ada Lovelace', 
         posts: [
-            {content: 'BILJETTKONTROLLANTER >:((((', hates: 23423}, 
-            {content: 'When you change to autumn jacket and forget your västtrafikkort in your summer jacket... at home', hates: 1234}
-            ],
+            {content: 'Long long long long long long long long long long long long long long long long long long long long long long long hate text for testing layout for long(er) text', hates: 0},
+            {content: "BILJETTKONTROLLANTER >:((((", hates: 23423}, 
+            {content: "When you change to autumn jacket and forget your västtrafikkort in your summer jacket... at home", hates: 1234},
+            {content: "Hello world! :D ..... not.", hates: 52}],
         profilePic: 'http://cpuboss.com/blog/wp-content/uploads/2013/10/Wikipedia_ada-lovelace_FullyC_001.jpg',
         userInfo: 'H8 ppl - LUV c0mput3rz'
         }
