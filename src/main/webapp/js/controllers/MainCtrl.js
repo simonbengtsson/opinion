03221797bd07b99d5d660944e6ad51ecd1d2f346
Simon Bengtsson <simonbengt@gmail.com>
2014-10-17 10:33:26 +0200

    Getting profile from twitter.

--- src/main/webapp/js/controllers/MainCtrl.js
+++ src/main/webapp/js/controllers/MainCtrl.js
@@ -1,6 +1,6 @@
 var app = angular.module('hage');
 
-app.controller('MainCtrl', ['$scope', 'ModelService', 'NetworkService', '$http', '$modal', function ($scope, model, network, $http, $modal) {
+app.controller('MainCtrl', ['$scope', 'ModelService', 'NetworkService', '$http', '$modal', '$location', function ($scope, model, network, $http, $modal, $location) {
 
     $scope.model = model;
 
@@ -17,7 +17,11 @@ app.controller('MainCtrl', ['$scope', 'ModelService', 'NetworkService', '$http',
         var mi = $modal.open({
             templateUrl: 'partials/create-modal.html',
             controller: function ($scope) {
-                $scope.post = {};
+                $scope.post = {
+                    text: '',
+                    picture: null,
+                    hatingUsers: []
+                };
                 $scope.create = function () {
                     $scope.$close($scope.post);
                 }
@@ -27,5 +31,16 @@ app.controller('MainCtrl', ['$scope', 'ModelService', 'NetworkService', '$http',
         mi.result.then(function (res) {
             model.posts.push(res);
         });
-    }
+    };
+
+    $scope.goToProfile = function () {
+        if (model.user) {
+            $location.path('profile');
+        } else {
+            $modal.open({
+                templateUrl: 'partials/login-modal.html'
+            });
+        }
+    };
+
 }]);
