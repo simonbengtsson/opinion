a3ad1614669b628b4f439aa74273dfecae53dc65
Simon Planhage <planhage@student.chalmers.se>
2014-10-17 15:39:55 +0200

    stek

--- src/main/java/com/dat076hage/hage/controllers/PostController.java
+++ src/main/java/com/dat076hage/hage/controllers/PostController.java
@@ -21,11 +21,11 @@ import javax.ws.rs.core.*;
  * @author stek
  */
 @Path("posts")
+@Consumes(value = MediaType.APPLICATION_JSON)
+@Produces(value = MediaType.APPLICATION_JSON)
 public class PostController {
  
     @GET
-    @Consumes(value = MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(value = {MediaType.APPLICATION_JSON}) 
     public String findAll(@QueryParam("username") String userName, @QueryParam("hagetag") String hageTag) {
         
         //TODO: temporary test, to be replaced
@@ -39,31 +39,23 @@ public class PostController {
     };
     
     @POST
-    @Consumes(value = {MediaType.APPLICATION_FORM_URLENCODED})
-    @Produces(value = {MediaType.APPLICATION_JSON})
     public String createPost(@QueryParam("text") String text) {
         return "Create new post with text: " + text;
     }
     
     @PUT
-    @Consumes(value = MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(value = {MediaType.APPLICATION_JSON})
     public String updatePost(@QueryParam("id") long postId, @QueryParam("text") String newText) {
         return "postId: " + postId + ", text: " + newText;
     }
     
     @DELETE
-    @Consumes(value = MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(value = {MediaType.APPLICATION_JSON})
     public String deletePost(@QueryParam("id") long postId) {
         return "post should be deleted: " + postId;
     }
     
     @GET
-    @Path(value = "single")
-    @Consumes(value = MediaType.APPLICATION_FORM_URLENCODED)
-    @Produces(value = {MediaType.APPLICATION_JSON})
-    public String findPost(@QueryParam("id") long id) {
+    @Path("{id}")
+    public String findPost(@PathParam("id") long id) {
         return "postId to be found: " + id;
     }
 }
