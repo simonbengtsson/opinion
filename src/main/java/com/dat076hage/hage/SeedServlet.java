47b906caffc958e38b54235ebdd723d98e0843d0
Simon Planhage <planhage@student.chalmers.se>
2014-10-25 17:27:26 +0200

    check

--- src/main/java/com/dat076hage/hage/SeedServlet.java
+++ src/main/java/com/dat076hage/hage/SeedServlet.java
@@ -47,47 +47,54 @@ public class SeedServlet extends HttpServlet {
     protected void processRequest(HttpServletRequest request, HttpServletResponse response)
             throws ServletException, IOException {
         
-        User kim = new User("kim", "My name is Kim Kling","sdfdsf", "sdfdsf", null);
-        User simonB = new User("simonb", "My name is Simon Bengtsson","sdfdsf", "sdfdsf", null);
-        User simonp = new User("simonp", "My name is Simon Planhage","sdfdsf", "sdfdsf", null);
-        User caroline = new User("caroline", "My name is Caroline Kabat","sdfdsf", "sdfdsf", null);
-        userReg.create(kim);
-        userReg.create(simonB);
-        userReg.create(simonp);
-        userReg.create(caroline);
+
+        if(userReg.find("kim") == null){
+          
+            User kim = new User("kim", "My name is Kim Kling", "", "", "");
+            User simonB = new User("simonb", "My name is Simon Bengtsson", "", "", "");
+            User simonP = new User("simonp", "My name is Simon Planhage", "", "", "");
+            User caroline = new User("caroline", "My name is Caroline Kabat", "", "", "");
+            userReg.create(kim);
+            userReg.create(simonB);
+            userReg.create(simonP);
+            userReg.create(caroline);
+
+            List<String> tags = new ArrayList<>();
+            tags.add("awesome");
+            tags.add("hashtags");
+
+            Post kimPost1 = new Post(kim, "This is my first, simple Post!");
+            Post kimPost2 = new Post(kim, "This is my second post, with link and position!", "", "http://feber.se", new ArrayList(), new GPS(57.689470, 11.973038));
+            Post kimPost3 = new Post(kim, "This is my third, #awesome post with #hashtags!", "", "", tags, null);
+            Post simonBPost1 = new Post(simonB, "This is my first, simple Post!");
+            Post simonBPost2 = new Post(simonB, "This is my second post, with link and position!", "", "http://feber.se", new ArrayList(), new GPS(57.689470, 11.973038));
+            Post simonBPost3 = new Post(simonB, "This is my third, #awesome post with #hashtags!", "", "", tags, null);
+            Post simonPPost1 = new Post(simonP, "This is my first, simple Post!");
+            Post simonPPost2 = new Post(simonP, "This is my second post, with link and position!", "", "http://feber.se", new ArrayList(), new GPS(57.689470, 11.973038));
+            Post simonPPost3 = new Post(simonP, "This is my third, #awesome post with #hashtags!", "", "", tags, null);
+            Post carolinePost1 = new Post(caroline, "This is my first, simple Post!");
+            Post carolinePost2 = new Post(caroline, "This is my second post, with link and position!", "", "http://feber.se", new ArrayList(), new GPS(57.689470, 11.973038));
+            Post carolinePost3 = new Post(caroline, "This is my third, #awesome post with #hashtags!", "", "", tags, null);
+            postReg.create(kimPost1);
+            postReg.create(kimPost2);
+            postReg.create(kimPost3);
+            postReg.create(simonBPost1);
+            postReg.create(simonBPost2);
+            postReg.create(simonBPost3);
+            postReg.create(simonPPost1);
+            postReg.create(simonPPost2);
+            postReg.create(simonPPost3);
+            postReg.create(carolinePost1);
+            postReg.create(carolinePost2);
+            postReg.create(carolinePost3);
+            
+            simonP.addFollowedUsers(kim);
+            simonP.addFollowedUsers(caroline);
+            simonP.addFollowedUsers(simonB);
+        }
+
         
-        List<String> tags = new ArrayList<>();
-        tags.add("awesome");
-        tags.add("hashtags");
         
-        Post kimPost1 = new Post(kim, "This is my first, simple Post!");
-        Post kimPost2 = new Post(kim, "This is my second post, with link and position!", "", "http://feber.se", new ArrayList(), new GPS(57.689470, 11.973038));
-        Post kimPost3 = new Post(kim, "This is my third, #awesome post with #hashtags!", "", "", tags, null);
-        Post simonBPost1 = new Post(simonB, "This is my first, simple Post!");
-        Post simonBPost2 = new Post(simonB, "This is my second post, with link and position!", "", "http://feber.se", new ArrayList(), new GPS(57.689470, 11.973038));
-        Post simonBPost3 = new Post(simonB, "This is my third, #awesome post with #hashtags!", "", "", tags, null);
-        Post simonPPost1 = new Post(simonp, "This is my first, simple Post!");
-        Post simonPPost2 = new Post(simonp, "This is my second post, with link and position!", "", "http://feber.se", new ArrayList(), new GPS(57.689470, 11.973038));
-        Post simonPPost3 = new Post(simonp, "This is my third, #awesome post with #hashtags!", "", "", tags, null);
-        Post carolinePost1 = new Post(caroline, "This is my first, simple Post!");
-        Post carolinePost2 = new Post(caroline, "This is my second post, with link and position!", "", "http://feber.se", new ArrayList(), new GPS(57.689470, 11.973038));
-        Post carolinePost3 = new Post(caroline, "This is my third, #awesome post with #hashtags!", "", "", tags, null);
-        postReg.create(kimPost1);
-        postReg.create(kimPost2);
-        postReg.create(kimPost3);
-        postReg.create(simonBPost1);
-        postReg.create(simonBPost2);
-        postReg.create(simonBPost3);
-        postReg.create(simonPPost1);
-        postReg.create(simonPPost2);
-        postReg.create(simonPPost3);
-        postReg.create(carolinePost1);
-        postReg.create(carolinePost2);
-        postReg.create(carolinePost3);
-        
-        simonp.addFollowedUsers(kim);
-        simonp.addFollowedUsers(caroline);
-        simonp.addFollowedUsers(simonB);
         
         response.setContentType("text/html;charset=UTF-8");
         try (PrintWriter out = response.getWriter()) {
